0bf461fe8437e16092ffbc47112e4d45f2d9e1da
Thomas Gomez <thomaseg2016@gmail,com>
2026-05-29 16:50:31 -0300

    terminando de cargas lo de las clases

---
+++ SisPersonal/Empleados.cs
@@ -0,0 +1,68 @@
+namespace EspacioEmpleado;
+
+//Enumeracion
+    public enum Cargos
+{
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
+}
+
+    public class Empleados
+    {
+        //Campos privados
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private char estadoCivil;
+        private DateTime fechaIngreso;
+        private double sueldo;
+        private Cargos cargo;
+
+        //Propiedades 
+        public string Nombre
+        {
+            get {return nombre;}
+            set {nombre = value;}
+        }
+
+        public string Apellido
+        {
+            get {return apellido;}
+            set {apellido = value;}
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get {return fechaNacimiento;}
+            set {fechaNacimiento = value;}
+        }
+        
+        public char EstadoCivil
+        {
+            get {return estadoCivil;}
+            set { estadoCivil = value;}
+        }
+
+        public DateTime FechaIngreso
+        {
+            get {return fechaIngreso;}
+            set {fechaIngreso = value;}
+        }
+
+        public double Sueldo
+        {
+            get { return sueldo;}
+            set {sueldo = value;}
+        }
+
+        public Cargos Cargo
+        {
+            get {return cargo;}
+            set {cargo = value;}
+        }
+
+    }
+
---
+++ SisPersonal/Program.cs
@@ -0,0 +1,18 @@
+﻿using EspacioEmpleado;
+
+    Empleados empleado1 = new Empleados();
+
+    empleado1.Nombre = "Thomas";
+    empleado1.Apellido = "Gomez";
+    empleado1.FechaNacimiento = new DateTime(2000, 2, 14);
+    empleado1.EstadoCivil = 'S';
+    empleado1.FechaIngreso = new DateTime(2026, 3 , 1);
+    empleado1.Sueldo = 250000;
+    empleado1.Cargo = Cargos.Ingeniero;
+
+    Console.WriteLine("---DATOS DEL EMPLEADO---");
+    Console.WriteLine("Nombre: " + empleado1.Nombre);
+    Console.WriteLine("Apellido: " + empleado1.Apellido);
+    Console.WriteLine("Cargo: " + empleado1.Cargo);
+    Console.WriteLine("Sueldo: " + empleado1.Sueldo);
+   
